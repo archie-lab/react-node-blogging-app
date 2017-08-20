417fa07bcd7861be0cf9a1ec1633c27379b65474
Artyom Shymukovich <arch817@gmail.com>
2017-08-20 13:30:13 +0800

    eslint

--- app.js
+++ app.js
@@ -1,87 +1,88 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const bodyParser = require('body-parser');
-const user = require('./user');
-const post = require('./post');
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const bodyParser = require('body-parser')
+const user = require('./user')
+const post = require('./post')
 
-const port = 7777;
-let sessions;
+const port = 7777
+let sessions
 
-const app = express();
-app.use(express.static(path.join(__dirname, 'html')));
-app.use(session({secret: 'my-secret'}));
+const app = express()
+app.use(express.static(path.join(__dirname, 'html')))
+app.use(session({secret: 'my-secret'}))
 
 app.listen(port, function () {
-  console.log("Started listening on port", port);
-});
+  console.log('Started listening on port', port)
+})
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/html/index.html');
-});
+  res.sendFile(__dirname + '/html/index.html')
+})
 
 app.get('/home', (req, res) => {
   if (sessions && sessions.username) {
-    res.sendFile(__dirname + '/html/home.html');
+    res.sendFile(__dirname + '/html/home.html')
   } else {
-    res.status(401);
-    res.send('unauthorized');
+    res.status(401)
+    res.send('unauthorized')
   }
-});
+})
 
 app.post('/signin', (req, res) => {
-  sessions = req.session;
-  let email = req.body.email;
-  let password = req.body.password;
-  console.log(`Sign in with email: ${email} and password: ${password}`);
+  sessions = req.session
+  let email = req.body.email
+  let password = req.body.password
+  console.log(`Sign in with email: ${email} and password: ${password}`)
   user.signin(email, password, (successLogin) => {
-        if (successLogin) {
-          sessions.username = email;
-          res.send('success');
-        } else {
-          res.status(500);
-          res.send('Wrong email of password');
-        }
+      if (successLogin) {
+        sessions.username = email
+        res.send('success')
+      } else {
+        res.status(500)
+        res.send('Wrong email of password')
       }
-  );
-});
+    }
+  )
+})
 
 app.post('/signup', (req, res) => {
-  let name = req.body.name;
-  let email = req.body.email;
-  let password = req.body.password;
+  let name = req.body.name
+  let email = req.body.email
+  let password = req.body.password
   if (name && email && password) {
-    console.log(`Sign up with name: ${name} email: ${email} and password: ${password}`);
-    user.signup(name, email, password);
-    res.send('success');
+    console.log(
+      `Sign up with name: ${name} email: ${email} and password: ${password}`)
+    user.signup(name, email, password)
+    res.send('success')
   } else {
-    res.status(500);
-    res.send('failure');
+    res.status(500)
+    res.send('failure')
   }
-});
+})
 
 app.post('/posts', (req, res) => {
-  let title = req.body.title;
-  let subject = req.body.subject;
-  post.addPost(title, subject, (result) => res.send(result));
-});
+  let title = req.body.title
+  let subject = req.body.subject
+  post.addPost(title, subject, (result) => res.send(result))
+})
 
 app.get('/posts', (req, res) => {
-  post.getPosts((result) => res.send(result));
-});
+  post.getPosts((result) => res.send(result))
+})
 
 app.get('/posts/:id', (req, res) => {
-  post.getPost(req.params.id, (result) => res.send(result));
-});
+  post.getPost(req.params.id, (result) => res.send(result))
+})
 
 app.put('/posts/:id', (req, res) => {
-  let title = req.body.title;
-  let subject = req.body.subject;
-  post.updatePost(req.params.id, title, subject, result => res.send(result));
-});
+  let title = req.body.title
+  let subject = req.body.subject
+  post.updatePost(req.params.id, title, subject, result => res.send(result))
+})
 
 app.delete('/posts/:id', (req, res) => {
-  post.deletePost(req.params.id, result => res.send(result));
-});
+  post.deletePost(req.params.id, result => res.send(result))
+})
--- html/home.jsx
+++ html/home.jsx
@@ -1,206 +1,205 @@
-var Router = window.ReactRouter.Router;
-var Route = window.ReactRouter.Route;
-var hashHistory = window.ReactRouter.hashHistory;
-var Link = window.ReactRouter.Link;
+var Router = window.ReactRouter.Router
+var Route = window.ReactRouter.Route
+var hashHistory = window.ReactRouter.hashHistory
 
 class ShowPost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {posts: []};
-    this.getPosts = this.getPosts.bind(this);
-    this.updatePost = this.updatePost.bind(this);
-    this.deletePost = this.deletePost.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = {posts: []}
+    this.getPosts = this.getPosts.bind(this)
+    this.updatePost = this.updatePost.bind(this)
+    this.deletePost = this.deletePost.bind(this)
   }
 
-  componentDidMount() {
-    this.getPosts();
+  componentDidMount () {
+    this.getPosts()
 
-    document.getElementById('homeHyperlink').className = 'active';
-    document.getElementById('addPostHyperlink').className = '';
+    document.getElementById('homeHyperlink').className = 'active'
+    document.getElementById('addPostHyperlink').className = ''
   }
 
-  getPosts() {
-    const self = this;
+  getPosts () {
+    const self = this
 
-    axios.get('/posts')
-        .then((response) => {
-          console.log('get posts response', response);
-          self.setState({posts: response.data});
-        })
-        .catch((error) => {
-          console.log('get posts error', error);
-        });
+    axios.get('/posts').then((response) => {
+      console.log('get posts response', response)
+      self.setState({posts: response.data})
+    }).catch((error) => {
+      console.log('get posts error', error)
+    })
   }
 
-  updatePost(id) {
-    console.log('update ', id);
-    hashHistory.push('/posts/' + id + '/edit');
+  updatePost (id) {
+    console.log('update ', id)
+    hashHistory.push('/posts/' + id + '/edit')
   }
 
-  deletePost(id) {
-    const self = this;
-    console.log('delete ', id);
-    axios.delete('/posts/' + id)
-        .then((response) => {
-          console.log(`post with id=${id} is deleted, response`, response)
-          self.getPosts();
-        })
-        .catch(error => console.log(error))
+  deletePost (id) {
+    const self = this
+    console.log('delete ', id)
+    axios.delete('/posts/' + id).then((response) => {
+      console.log(`post with id=${id} is deleted, response`, response)
+      self.getPosts()
+    }).catch(error => console.log(error))
   }
 
-
-  render() {
-    const self = this;
+  render () {
+    const self = this
     return (
-        <table className="table table-striped">
-          <thead>
+      <table className="table table-striped">
+        <thead>
           <tr>
             <th>#</th>
             <th>Title</th>
             <th>Subject</th>
-            <th/>
-            <th/>
+            <th />
+            <th />
           </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
           {
-            this.state.posts.map(function (post, index) {
-              return <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{post.title}</td>
-                <td>{post.subject}</td>
-                <td>
-                  <button onClick={() => self.updatePost(post._id)} className="btn btn-link">
-                    <span className="glyphicon glyphicon-pencil"/>
-                  </button>
-                </td>
-                <td>
-                  <button onClick={() => self.deletePost(post._id)} className="btn btn-link">
-                    <span className="glyphicon glyphicon-remove"/>
-                  </button>
-                </td>
-              </tr>
-            })
-          }
-          </tbody>
-        </table>
-    );
+          this.state.posts.map(function (post, index) {
+            return <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{post.title}</td>
+              <td>{post.subject}</td>
+              <td>
+                <button onClick={() => self.updatePost(post._id)}
+                        className="btn btn-link">
+                  <span className="glyphicon glyphicon-pencil" />
+                </button>
+              </td>
+              <td>
+                <button onClick={() => self.deletePost(post._id)}
+                        className="btn btn-link">
+                  <span className="glyphicon glyphicon-remove" />
+                </button>
+              </td>
+            </tr>
+          })
+        }
+        </tbody>
+      </table>
+    )
   }
 }
 
 class AddPost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: '', subject: '', id: ''};
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleSubjectChange = this.handleSubjectChange.bind(this);
-    this.getPostWithId = this.getPostWithId.bind(this);
-    this.addPost = this.addPost.bind(this);
-    this.updatePost = this.updatePost.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = {title: '', subject: '', id: ''}
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleSubjectChange = this.handleSubjectChange.bind(this)
+    this.getPostWithId = this.getPostWithId.bind(this)
+    this.addPost = this.addPost.bind(this)
+    this.updatePost = this.updatePost.bind(this)
   }
 
-  componentDidMount() {
-    document.getElementById('homeHyperlink').className = '';
-    document.getElementById('addPostHyperlink').className = 'active';
+  componentDidMount () {
+    document.getElementById('homeHyperlink').className = ''
+    document.getElementById('addPostHyperlink').className = 'active'
 
     if (this.props.params.id) {
-      this.getPostWithId();
+      this.getPostWithId()
     }
-
   }
 
-  handleTitleChange(e) {
-    this.setState({title: e.target.value});
+  handleTitleChange (e) {
+    this.setState({title: e.target.value})
   }
 
-  handleSubjectChange(e) {
-    this.setState({subject: e.target.value});
+  handleSubjectChange (e) {
+    this.setState({subject: e.target.value})
   }
 
-  getPostWithId() {
-    const id = this.props.params.id;
-    const self = this;
-    axios.get('/posts/' + id)
-        .then(function (response) {
-          console.log(`get post by id=${id} response:`, response);
-          if (response) {
-            self.setState({title: response.data.title, subject: response.data.subject, id: response.data._id});
-          }
+  getPostWithId () {
+    const id = this.props.params.id
+    const self = this
+    axios.get('/posts/' + id).then(function (response) {
+      console.log(`get post by id=${id} response:`, response)
+      if (response) {
+        self.setState({
+          title: response.data.title,
+          subject: response.data.subject,
+          id: response.data._id
         })
-        .catch(function (error) {
-          console.log(`get post by id=${id} error:`, error);
-        });
+      }
+    }).catch(function (error) {
+      console.log(`get post by id=${id} error:`, error)
+    })
   }
 
-  addPost() {
+  addPost () {
     axios.post('/posts', {
       title: this.state.title,
       subject: this.state.subject
     }).then((response) => {
-      console.log('addPost response', response);
-      hashHistory.push('/');
+      console.log('addPost response', response)
+      hashHistory.push('/')
     }).catch((error) => {
-      console.log('addPost error', error);
+      console.log('addPost error', error)
     })
   }
 
-  updatePost() {
+  updatePost () {
     axios.put('/posts/' + this.state.id, {
       title: this.state.title,
       subject: this.state.subject
     }).then((response) => {
-      console.log('updatePost response', response);
-      hashHistory.push('/');
+      console.log('updatePost response', response)
+      hashHistory.push('/')
     }).catch((error) => {
-      console.log('updatePost error', error);
+      console.log('updatePost error', error)
     })
   }
 
-  render() {
-    const hasId = this.state.id && this.state.id.length > 0;
+  render () {
+    const hasId = this.state.id && this.state.id.length > 0
     return (
-        <div className="col-md-5">
-          <div className="form-area">
-            <form role="form">
-              <br/>
-              <div className="form-group">
-                <input type="text"
-                       className="form-control"
-                       id="title"
-                       name="title"
-                       value={this.state.title}
-                       onChange={this.handleTitleChange}
-                       placeholder="Title" required/>
-              </div>
+      <div className="col-md-5">
+        <div className="form-area">
+          <form role="form">
+            <br />
+            <div className="form-group">
+              <input type="text"
+                     className="form-control"
+                     id="title"
+                     name="title"
+                     value={this.state.title}
+                     onChange={this.handleTitleChange}
+                     placeholder="Title" required />
+            </div>
 
-              <div className="form-group">
-                <textarea className="form-control"
+            <div className="form-group">
+              <textarea className="form-control"
                           type="textarea"
                           id="subject"
                           placeholder="Subject"
                           value={this.state.subject}
                           onChange={this.handleSubjectChange}
                           maxLength="140"
-                          rows="7"></textarea>
-              </div>
+                          rows="7" />
+            </div>
 
-              <button type="button"
-                      id="submit"
-                      name="submit"
-                      className="btn btn-primary pull-right"
-                      onClick={hasId ? this.updatePost : this.addPost}>{hasId ? 'Update' : 'Add'} Post
-              </button>
-            </form>
-          </div>
+            <button type="button"
+                    id="submit"
+                    name="submit"
+                    className="btn btn-primary pull-right"
+                    onClick={hasId ? this.updatePost : this.addPost}>{hasId
+              ? 'Update'
+              : 'Add'} Post
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    )
   }
 }
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-      <Route component={ShowPost} path="/"/>
-      <Route component={AddPost} path="/posts/add"/>
-      <Route component={AddPost} path="/posts/:id/edit"/>
-    </Router>,
-    document.getElementById('app'));
+  <Router history={hashHistory}>
+    <Route component={ShowPost} path="/" />
+    <Route component={AddPost} path="/posts/add" />
+    <Route component={AddPost} path="/posts/:id/edit" />
+  </Router>,
+  document.getElementById('app'))
